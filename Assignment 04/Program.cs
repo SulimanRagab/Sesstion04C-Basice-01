cffb0de4ed0b562de434bea07ca889423d012146
Suliman <sulimanrageb015@gmail.com>
2026-07-27 23:04:54 +0300

    Q02

--- Assignment 04/Program.cs
+++ Assignment 04/Program.cs
@@ -7,3 +7,16 @@
 //Console.WriteLine(prices[1]);
 #endregion
 
+#region Q02
+//Requirements
+//Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+//shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+//int[,] shelfCopies =
+//{
+//{3,5},
+//{1,4}
+//};
+//Console.WriteLine(shelfCopies[0,1]); //  Print   5
+#endregion
+
+
