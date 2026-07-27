e94fc99a3a03b305acb9e66ba877b0e07cb7c6b9
Suliman <sulimanrageb015@gmail.com>
2026-07-27 23:04:26 +0300

    ََQ01

--- Assignment 04/Program.cs
+++ Assignment 04/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+#region Q01
+//Requirements
+//Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print
+//the second price (index 1).
+//double[] prices = { 25.5, 40.0, 33.75 };
+//Console.WriteLine(prices[1]);
+#endregion
+
